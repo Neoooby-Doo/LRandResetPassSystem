409df9db15b5862ae14d05e1e88dbec4704dbfe1
Sakhawat Hossain <sakhwt.hssain@gmail.com>
2024-02-28 22:58:33 +0600

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
  /* If user is not logged in, redirect to `login.php` */
 
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
-  echo "<script>" . "window.location.href='./login.php';" . "</script>";
+  echo "<script>" . "window.location.href='./login';" . "</script>";
   exit;
 }
 
