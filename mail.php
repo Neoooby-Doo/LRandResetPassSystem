8945ede1f10bea41dd0c409c39e2cfaa501a70f8
Sakhawat Hossain <sakhwt.hssain@gmail.com>
2024-11-17 12:26:36 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,39 +1,38 @@
 <?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
-  #Require some files from PHPMailer
-  
-  require 'PHPMailer/src/Exception.php';
-  require 'PHPMailer/src/PHPMailer.php';
-  require 'PHPMailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-function send_mail($recipient,$subject,$message)
+// Include PHPMailer files
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+function send_mail($recipient, $subject, $message)
 {
+    $mail = new PHPMailer();
+    $mail->isSMTP();
 
-  $mail = new PHPMailer();
-  $mail->IsSMTP();
+    // SMTP settings
+    $mail->SMTPDebug  = 0; // Debugging off for production (use 2 for verbose debugging)
+    $mail->SMTPAuth   = true; // Enable SMTP authentication
+    $mail->SMTPSecure = 'tls'; // Encryption type (use 'ssl' if required)
+    $mail->Port       = 587; // SMTP port (use 465 for SSL)
+    $mail->Host       = '...'; // SMTP server
+    $mail->Username   = '...'; // Your full email address
+    $mail->Password   = '...'; // E-mail Password (N.B. App Password for Gmail)
 
-  $mail->SMTPDebug  = 0;  
-  $mail->SMTPAuth   = TRUE;
-  $mail->SMTPSecure = "tls";
-  $mail->Port       = 587;
-  $mail->Host       = "smtp.gmail.com";
-  $mail->Username   = "..."; #Enter E-mail Address
-  $mail->Password   = "..."; #Enter App Password
+    // Email settings
+    $mail->isHTML(true); // Send email in HTML format
+    $mail->addAddress($recipient, "User"); // Recipient's email and name
+    $mail->setFrom('....', '....'); // Sender's email and name
+    $mail->Subject = $subject; // Email subject
+    $mail->Body    = $message; // Email content
 
-  $mail->IsHTML(true);
-  $mail->AddAddress($recipient, "User");
-  $mail->SetFrom("...", "..."); 
-  $mail->Subject = $subject;
-  $content = $message;
-
-  $mail->MsgHTML($content); 
-  if(!$mail->Send()) {
-    return false;
-  } else {
-    return true;
-  }
-
+    // Send email and return status
+    if (!$mail->send()) {
+        return false;
+    } else {
+        return true;
+    }
 }
-
 ?>
